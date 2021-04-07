6ef272b0bbdbe98320b23d2879df0068b6ea08a9
kingbraga10 <ricardo.esteves.rodrigues@tecnico.ulisboa.pt>
2021-04-07 14:09:59 +0100

    lelo

--- t2/mat/master.m
+++ t2/mat/master.m
@@ -9,18 +9,10 @@ theoretical1
 
 data2circuitfile1
 
-data2circuitfile2
-
 theoretical2
 
-data2circuitfile3
-
-data2circuitfile4
+data2circuitfile2
 
 theoretical3
 
-theoretical4
 
-theoretical5
-
-theoretical6
